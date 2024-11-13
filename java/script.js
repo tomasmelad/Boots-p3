3a588f37fe0e355f497184d3dbdd281ad497fd0e
tomasmelad <108224154+tomasmelad@users.noreply.github.com>
2024-11-13 16:18:42 +0200

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,13 @@
+var x = document.getElementById("nnn")
+x.onclick = function(){
+alert("Text Copied")
+}
+var y = document.getElementById("nn")
+y.onclick = function(){
+alert("Text Copied")
+}
+var y = document.getElementById("l")
+y.onclick = function(){
+alert("Text Copied")
+}
+
